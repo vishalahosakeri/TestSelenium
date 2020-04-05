86746a544009a76f8f1d36841184dcf2d73e34ee
sanvijanvi <sanvijanvi@sanvijanvis-mbp.lan>
2020-04-04 23:23:24 -0700

    Checking repository push

--- QAF_amazon/src/com/qaf/amz/AmazonQAF.java
+++ QAF_amazon/src/com/qaf/amz/AmazonQAF.java
@@ -2,10 +2,13 @@ package com.qaf.amz;
 
 import org.testng.annotations.Test;
 import static com.qmetry.qaf.automation.step.CommonStep.get;
-
 import com.qaf.amzServices.AmazonServiceImpl;
 import com.qaf.amzServices.AmazonServices;
 import com.qmetry.qaf.automation.ui.WebDriverTestCase;
+
+/*
+ * Calling amazon services
+ */
 
 public class AmazonQAF extends WebDriverTestCase{
 	
